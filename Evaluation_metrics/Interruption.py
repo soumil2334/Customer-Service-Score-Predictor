525eaa112fcb4061b797d01b4f63a3e364baec0e
Soumil Sinha <soumil2433@gmail.com>
2026-01-03 19:19:20 +0530

    Changes Committed

--- Evaluation_metrics/Interruption.py
+++ Evaluation_metrics/Interruption.py
@@ -4,7 +4,7 @@ def interuptions(corrected_utterances):
     interuption_bool=False
     for i,u in enumerate(corrected_utterances):
         if i+1 < len(corrected_utterances) and u.get('speaker')=='Customer' and corrected_utterances[i+1]['speaker']=='Customer Service Agent':
-            if corrected_utterances[i+1]['start']-u.get('end')<300:
+            if corrected_utterances[i+1]['start']-u.get('end')<150:
                 interuption_bool=True
                 
     return interuption_bool
